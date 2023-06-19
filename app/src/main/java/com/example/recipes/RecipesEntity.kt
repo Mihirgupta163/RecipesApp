3b9ab0c6eb6730c0284aba5b26fe234cc779745c
mahi <mihirgupta163@gmail.com>
2023-06-19 22:00:03 +0530

    Room Dependency added, Recipes Entity Class created.

---
+++ app/src/main/java/com/example/recipes/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com.example.recipes
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.recipes.models.FoodRecipe
+import com.example.recipes.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+data class RecipesEntity (val foodRecipe: FoodRecipe){
+
+    @PrimaryKey(autoGenerate = false)
+    var id:Int = 0
+
+}

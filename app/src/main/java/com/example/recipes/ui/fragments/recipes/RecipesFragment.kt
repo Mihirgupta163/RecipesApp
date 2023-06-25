713232ae71204debe9b042867475238115d8efe4
mahi <mihirgupta163@gmail.com>
2023-06-25 22:03:39 +0530

    Bottom Sheet is added to filter the Recipes. But only the UI is being created.

--- app/src/main/java/com/example/recipes/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/recipes/ui/fragments/recipes/RecipesFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cooltechworks.views.shimmer.ShimmerRecyclerView
 import com.example.recipes.viewmodels.MainViewModel
@@ -47,8 +48,11 @@ class RecipesFragment : Fragment() {
         binding.mainViewModel = mainViewModel
 
         setUpRecyclerView()
-
         readDatabase()
+
+        binding.recipesFab.setOnClickListener{
+            findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
+        }
 
         return binding.root
     }

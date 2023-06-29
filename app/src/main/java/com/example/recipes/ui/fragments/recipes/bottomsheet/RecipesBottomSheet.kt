4681165e02fc878740dbb4e51ad0312dfd23ed22
mahi <mihirgupta163@gmail.com>
2023-06-29 22:27:09 +0530

    DataStore is injected at recipesViewModel Class.

--- app/src/main/java/com/example/recipes/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
+++ app/src/main/java/com/example/recipes/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -4,17 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.recipes.R
+import com.example.recipes.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.recipes.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.example.recipes.viewmodels.RecipesViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 
 class RecipesBottomSheet : BottomSheetDialogFragment() {
+    private lateinit var recipesViewModel: RecipesViewModel
 
+    private var mealTypeChip = DEFAULT_MEAL_TYPE
+    private var mealTypeChipId = 0
+    private var dietTypeChip = DEFAULT_DIET_TYPE
+    private var dietTypeChipId = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipesViewModel = ViewModelProvider(requireActivity())[RecipesViewModel::class.java]
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+        val mView = inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+
+        mView.findViewById<ChipGroup>(R.id.mealType_chipGroup).setOnCheckedStateChangeListener(){ group, selectedChipId ->
+            val chip = group.findViewById<Chip>(group.checkedChipId)
+        }
+
+        return mView
     }
 }

20a54456013e34b8a269988961668ddcfaab1cf6
mahi <mihirgupta163@gmail.com>
2023-07-09 19:48:30 +0530

    Details Activity Created.

---
+++ app/src/main/java/com/example/recipes/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recipes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recipes.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}

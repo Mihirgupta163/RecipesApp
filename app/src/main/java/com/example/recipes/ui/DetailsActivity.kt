dd00f49131870df1b4dfbb4f2ec8ca505b7b0315
mahi <mihirgupta163@gmail.com>
2023-07-12 21:51:05 +0530

    Tab Layout is added to DetailsActivity.

--- app/src/main/java/com/example/recipes/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/recipes/ui/DetailsActivity.kt
@@ -5,19 +5,57 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavArgs
+import androidx.navigation.navArgs
+import androidx.viewpager2.widget.ViewPager2
 import com.example.recipes.R
+import com.example.recipes.adapters.PagerAdapter
+import com.example.recipes.ui.fragments.ingredients.IngredientsFragment
+import com.example.recipes.ui.fragments.instructions.InstructionsFragment
+import com.example.recipes.ui.fragments.overview.OverviewFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailsActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
+    private val args by navArgs<DetailsActivityArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
         toolbar = findViewById<Toolbar>(R.id.toolbar)
+        viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        tabLayout = findViewById<TabLayout>(R.id.tabLayout)
 
         setSupportActionBar(toolbar)
         toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val title = ArrayList<String>()
+        title.add("Overview")
+        title.add("Ingredients")
+        title.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle",args.result)
+
+        val adapter = PagerAdapter(resultBundle,this,fragments)
+
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(
+            tabLayout, viewPager
+        ){ tab, position -> tab.text = title[position] }.attach()
 
     }
 
